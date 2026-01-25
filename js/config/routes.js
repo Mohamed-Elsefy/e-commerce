ceda4ce747001a270877e4f3ff4cb14dfccee5e5
osama <oe2044960@gmail.com>
2026-01-25 22:08:07 +0200

    Merge branch 'main' of https://github.com/Mohamed-Elsefy/e-commerce

---
+++ js/config/routes.js
@@ -0,0 +1,30 @@
+export const routes = {
+    home: {
+        html: "/html/home.html",
+        js: "/js/pages/home.js",
+    },
+    products: {
+        html: "/html/products.html",
+        js: "/js/pages/products.js",
+    },
+    product: {
+        html: "/html/product_details.html",
+        js: "/js/pages/product_details.js",
+    },
+    cart: {
+        html: "/html/cart.html",
+        js: "/js/pages/cart.js",
+    },
+    chatbot: {
+        html: "/html/chatbot.html",
+        js: "/js/pages/chatbot.js",
+    },
+    login: {
+        html: "/html/login.html",
+        js: "/js/pages/login.js",
+    },
+    register: {
+        html: "/html/register.html",
+        js: "/js/pages/register.js",
+    },
+};
---
+++ js/services/product_services.js
@@ -0,0 +1,53 @@
+export async function getAllProducts() {
+    const response = await fetch(`./data/product.json`)
+    const products = await response.json()
+    return products
+}
+
+// get product by id
+export async function getProductById(id) {
+    const products = await getAllProducts()
+    const product = products.find(product => product.id == id)
+    return product
+}
+
+// get all reviews
+export async function getAllReviews() {
+    const response = await fetch(`./data/reviews.json`)
+    const reviews = await response.json()
+    return reviews
+}
+// get product by count
+export async function getProductByCount(start, end) {
+    const products = await getAllProducts()
+    const product = products.slice(start, end)
+    return product
+}
+
+// get product reviews
+export async function getProductReviews(productId) {
+    const reviews = await getAllReviews()
+    const productReviews = reviews.filter(review => review.productId == productId)
+    return productReviews
+}
+
+// get discount
+export async function getDiscount(productId) {
+    const product = await getProductById(productId)
+    const discount = product.discount
+    return discount
+}
+
+
+//get cart
+export async function getCart() {
+    const cart = localStorage.getItem('cart')
+    return cart ? JSON.parse(cart) : []
+}
+
+
+//get product id from url
+export function getProductId() {
+    const hash = location.hash.split('?')[1];
+    return new URLSearchParams(hash).get('id');
+}
