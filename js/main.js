e0f9deddcae70bf61ecdb88cff90dedc78b3b71a
osama <oe2044960@gmail.com>
2026-01-28 14:29:25 +0200

    add invoice

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@ import { routes } from "./config/routes.js";
 import { initTheme } from './Utilites/theme.js';
 import { renderAuthButtons } from './Utilites/renderAuthButtons.js';
 import * as authService from './services/auth_services.js';
+// import * as checkout from './services/checkout.js';
+
+// checkout.setOrderDefault();
 
 // Reference to the currently loaded page script
 let currentScript;
